e76d960ad0350f42c225541bcdc602094f122fbc
jony <37026036+jonyKhalon@users.noreply.github.com>
2024-01-30 12:26:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Note</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
